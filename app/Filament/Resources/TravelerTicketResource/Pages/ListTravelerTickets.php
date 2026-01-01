c2554361007017e01ee8d326ee1030bfd0d4825b
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-01 12:33:24 +0200

    asdsad

--- app/Filament/Resources/TravelerTicketResource/Pages/ListTravelerTickets.php
+++ app/Filament/Resources/TravelerTicketResource/Pages/ListTravelerTickets.php
@@ -13,7 +13,7 @@ class ListTravelerTickets extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //
         ];
     }
 }
