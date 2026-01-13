e1e964af5e8c5f390e3a42f4a1d5709cbe560b93
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-13 15:23:31 +0200

    wqewqe

--- resources/views/filament/pages/ticket-package-control-center.blade.php
+++ resources/views/filament/pages/ticket-package-control-center.blade.php
@@ -1,21 +1,27 @@
 <x-filament-panels::page>
-    <x-filament::tabs>
-        <x-filament::tabs.item
-            :href="request()->fullUrlWithQuery(['tab' => 'tickets'])"
-            wire:navigate
-            :active="request('tab', 'tickets') === 'tickets'"
-        >
-            Tickets
-        </x-filament::tabs.item>
+    <div class="border-b border-gray-200 dark:border-gray-700">
+        <nav class="-mb-px flex gap-6">
+            <a
+                href="{{ request()->fullUrlWithQuery(['tab' => 'tickets']) }}"
+                class="py-3 text-sm font-medium border-b-2
+                    {{ request('tab', 'tickets') === 'tickets'
+                        ? 'border-primary-600 text-primary-600'
+                        : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }}"
+            >
+                Tickets
+            </a>
 
-        <x-filament::tabs.item
-            :href="request()->fullUrlWithQuery(['tab' => 'packages'])"
-            wire:navigate
-            :active="request('tab', 'tickets') === 'packages'"
-        >
-            Packages
-        </x-filament::tabs.item>
-    </x-filament::tabs>
+            <a
+                href="{{ request()->fullUrlWithQuery(['tab' => 'packages']) }}"
+                class="py-3 text-sm font-medium border-b-2
+                    {{ request('tab', 'tickets') === 'packages'
+                        ? 'border-primary-600 text-primary-600'
+                        : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' }}"
+            >
+                Packages
+            </a>
+        </nav>
+    </div>
 
     <div class="mt-4" wire:key="tab-{{ request('tab', 'tickets') }}">
         {{ $this->table }}
