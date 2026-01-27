094ff16454523bf0839c3cbc45646a7c860afbf8
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-27 14:48:05 +0200

    eqw

--- app/Http/Controllers/Api/SenderAuthController.php
+++ app/Http/Controllers/Api/SenderAuthController.php
@@ -260,7 +260,7 @@ class SenderAuthController extends BaseApiController
             $this->deviceRepository->createOrUpdate([
                 'sender_id' => $sender->id,
                 'device_id' => $request->device_id,
-                'fcm_token' => $request->fcm_token,
+                'fcm_token' => $request->fcm,
                 'device_type' => $request->device_type,
                 'device_name' => $request->device_name,
             ]);
